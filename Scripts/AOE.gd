f826ad43e5a81ffe691f012f86603d43e609ef23
neverix <nev@neverix.io>
2020-07-17 14:19:53 +0300

    добавил расстояние в aoe

--- Scripts/AOE.gd
+++ Scripts/AOE.gd
@@ -1,6 +1,10 @@
 extends Node2D
 
 export var speed = 10
+export var min_distance = 100
+export var max_distance = 300
 
 func _process(delta):
-	GameManager.player.health.damage(delta * speed)
+	var distance = GameManager.player.position.distance_to(position)
+	if min_distance < distance and distance < max_distance:
+		GameManager.player.health.damage(delta * speed)
