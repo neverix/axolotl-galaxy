6d9fa71300ae1550feade2e9c989ab8186f9b102
neverix <nev@neverix.io>
2020-07-17 14:34:55 +0300

    добавил перемещение между уровнями

--- Scripts/GameManager.gd
+++ Scripts/GameManager.gd
@@ -1,13 +1,26 @@
 extends Node
 
 var player = null
-var level = "res://Levels/Test.tscn"
+var level_prefix = "res://Scenes/"
+var level_suffix = ".tscn"
+var levels = [
+	"Test",
+	"Menu"
+]
+var level = 0
+
+func level(index):
+	return level_prefix + levels[index] + level_suffix
 
 func death():
 	game()
 
 func game():
-	load_scene(level)
+	load_scene(level(level))
+
+func win():
+	level += 1
+	game()
 
 func menu():
 	load_scene("res://Scenes/Menu.tscn")
--- Scripts/Win.gd
+++ Scripts/Win.gd
@@ -1,7 +1,5 @@
 extends Area2D
 
-export var damage = 20
-
 func _process(delta):
 	if overlaps_body(GameManager.player):
-		GameManager.player.health.damage(delta * damage)
+		GameManager.win()
