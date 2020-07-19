a7005aaf010b0b987221bfb233e810d8aba0844a
neverix <nev@neverix.io>
2020-07-19 07:01:21 +0300

    попробуй

--- Scripts/Camera.gd
+++ Scripts/Camera.gd
@@ -1,9 +1,5 @@
 extends Camera2D
 
 func _process(_delta):
-	if (GameManager.cur_player.position.x > 450
-	and GameManager.cur_player.position.x < 1360):
-		position = Vector2(GameManager.cur_player.position.x, position.y)
-	if (GameManager.cur_player.position.y < 710
-	and GameManager.cur_player.position.y > 170):
-		position = Vector2(position.x, GameManager.cur_player.position.y)
+	position.x = min(max(GameManager.cur_player.position.x, 450), 1360)
+	position.y = min(max(GameManager.cur_player.position.y, 170), 710)
